2c548375b6dd02339b461e473a4408150538faa2
jaimesoaduni <jaime.acosta3@utp.ac.pa>
2022-08-31 17:45:24 -0500

    drawCircle

--- Parte1_Proyecto1.py
+++ Parte1_Proyecto1.py
@@ -1,8 +1,41 @@
 from turtle import *
+from math import *
 
 
 #TODO: algoritmo que genere un circulo
-def drawCircle():
+# Dibuja un círculo
+# Toma como referencia el centro tanto en x como en y, y el radio,
+# luego dibuja el círculo con las parámetros dados.
+def drawCircle(centroX: int, centroY: int, radio: int):
+    # Fórmula para generar el círculo:
+    # y = k +- sqrt(r^2 - (x-h)^2)
+
+    Dominio = {
+        "min": centroX - radio,
+        "max": 0
+    }
+    Dominio["max"] = Dominio["min"] + 2 * radio
+    x, y = 0, 0
+
+    # Eje x:
+    h = centroX
+
+    # Eje y:
+    k = centroY
+
+    up()
+    goto(Dominio["min"], centroY)
+    down()
+
+    # Parte superior
+    for x in range(Dominio["min"], Dominio["max"]+1):
+        y = k + sqrt(radio**2 - (x-h)**2)
+        goto(x, y)
+
+    # Parte inferior
+    for x in reversed(range(Dominio["min"], Dominio["max"]+1)):
+        y = k - sqrt(radio**2 - (x-h)**2)
+        goto(x, y)
     pass
 
 
