d8829ffc888f6834250dc5232b647dc1ef3ba826
pablo-6728 <51794965+pablo-6728@users.noreply.github.com>
2022-08-31 16:15:07 -0500

    Creacion del esqueleto para trabajar el proyecto

---
+++ Parte1_Proyecto1.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+
+#TODO: algoritmo que genere un circulo
+def drawCircle():
+    pass
+
+
+#TODO: algoritmo que genere un cuadrado
+def drawSquare():
+    #para esta parte tenemos que hacer 4 lineas usando el algoritmo DDA
+    pass
+
+
+#Eje de coordenadas
+def drawCartesianPlane():
+    pass
