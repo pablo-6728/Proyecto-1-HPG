d65abaffbce68cc75b5551691252fa9b006c9716
pablo-6728 <51794965+pablo-6728@users.noreply.github.com>
2022-09-05 14:02:00 -0500

    dibujo del cuadrado para rellenar

--- functions.js
+++ functions.js
@@ -1,7 +1,9 @@
 function setup() {
-  createCanvas(400,400);
+	createCanvas(400, 400);
 }
 
 function draw() {
-
+	//dibujo de un cuadrado utilizando p5. Los bordes son negros para aplicar el algoritmo FloodFill en el coloreo
+	//los primeros parametros son la pos y el ultimo es el tamaño
+	square(100,100,200)
 }
