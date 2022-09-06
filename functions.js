e230e14d19842d080425cd9d323e803222800ecd
pablo-6728 <51794965+pablo-6728@users.noreply.github.com>
2022-09-06 11:52:30 -0500

    Flood semifuncional

--- functions.js
+++ functions.js
@@ -7,11 +7,11 @@ function setup() {
 function draw() {
 	//dibujo de un cuadrado utilizando p5. Los bordes son negros para aplicar el algoritmo FloodFill en el coloreo
 	//los primeros parametros son la pos y el ultimo es el tamaño
-	cuadrado = square(0,0,200)
+	cuadrado = square(0,0,100)
 	//getPixelArray(cuadrado)
 	console.log(cuadrado.get(50 , 50))
 	let bg = cuadrado.get(50 , 50)
-	FloodFill(50, 50, bg)
+	FloodFill(5, 25, bg)
 
 	//agregar un floodfill aqu?
 	
@@ -47,7 +47,7 @@ function FloodFill(x, y, bg){
 	//coordenadas
 	let pix = get(x, y)
 	console.log(pix)
-	if (pix === bg){
+	if (verificarColor(bg, pix)){
 		console.log('Iniciando floodfill en el ' + x + ', ' + y)
 		set(x, y, color(0))
 		FloodFill(x + 1, y, bg)
@@ -58,4 +58,8 @@ function FloodFill(x, y, bg){
 	}
 	updatePixels()
 
+}
+
+function verificarColor(arr1, arr2){
+	return arr1[0]==arr2[0] && arr1[1]==arr2[1] && arr1[2]==arr2[2] && arr1[3]==arr2[3]
 }
