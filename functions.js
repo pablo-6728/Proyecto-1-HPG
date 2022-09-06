a88a25894673c2d9fa0a23766e41326e96830e66
pablo-6728 <51794965+pablo-6728@users.noreply.github.com>
2022-09-06 10:10:08 -0500

    Floodfill pero no funcional

--- functions.js
+++ functions.js
@@ -8,12 +8,13 @@ function draw() {
 	//dibujo de un cuadrado utilizando p5. Los bordes son negros para aplicar el algoritmo FloodFill en el coloreo
 	//los primeros parametros son la pos y el ultimo es el tamaño
 	cuadrado = square(100,100,200)
-	FloodFill(cuadrado)
+	getPixelArray(cuadrado)
+	//FloodFill(50, 25, cuadrado)
 	noLoop()
 }
 
 //TODO: hacer la funcion de relleno de color
-function FloodFill(shape){
+function getPixelArray(shape){
 	//variables de posicion
 	let x, y, pix
 	//let pix = shape.get(0, 0)
@@ -28,6 +29,23 @@ function FloodFill(shape){
 		}
 	}
 	//console.log(color[1])
-	console.log(color)
-	return;
+	//console.log(color)
+	FloodFill(50, 25, color[0])
+
+}
+
+function FloodFill(x, y, coloraArray){
+	let c = color(255, 204, 0) 
+	console.log('Iniciando floodfill')
+	console.log('Floodfill en ' + x + ', ' + y)
+	if(coloraArray[0] == [0, 0, 0, 0]){
+		set(x, y, c)
+		FloodFill(x+1, y, coloraArray[x + 1])
+		FloodFill(x-1, y, coloraArray[x + 1])
+		FloodFill(x, y+1, coloraArray[x + 1])
+		FloodFill(x, y-1, coloraArray[x + 1])
+
+		updatePixels()
+	}
+	
 }
