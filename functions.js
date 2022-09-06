9f812afc7c0299f69bddb6fb8bcce7a07335aadc
pablo-6728 <51794965+pablo-6728@users.noreply.github.com>
2022-09-06 09:23:26 -0500

    Guardar la informacion de los pixeles

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+let cuadrado
+
 function setup() {
 	createCanvas(400, 400);
 }
@@ -5,10 +7,27 @@ function setup() {
 function draw() {
 	//dibujo de un cuadrado utilizando p5. Los bordes son negros para aplicar el algoritmo FloodFill en el coloreo
 	//los primeros parametros son la pos y el ultimo es el tamaño
-	square(100,100,200)
+	cuadrado = square(100,100,200)
+	FloodFill(cuadrado)
+	noLoop()
 }
 
 //TODO: hacer la funcion de relleno de color
-function FloodFill(){
+function FloodFill(shape){
+	//variables de posicion
+	let x, y, pix
+	//let pix = shape.get(0, 0)
+	let color = []
+	
 
+	//asignar los valores al array de color
+	for (x = 0; x < 100; x++){
+		for (y = 0; y < 100; y++){
+			pix = shape.get(x, y)
+			color[x] = pix
+		}
+	}
+	//console.log(color[1])
+	console.log(color)
+	return;
 }
