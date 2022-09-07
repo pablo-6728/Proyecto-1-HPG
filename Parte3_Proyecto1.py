b842340e3e09fd0e1ee74508c66a9110f276c3e8
pablo-6728 <51794965+pablo-6728@users.noreply.github.com>
2022-09-07 11:23:59 -0500

    Creacion de documento de python con funcion move

---
+++ Parte3_Proyecto1.py
@@ -0,0 +1,45 @@
+from turtle import *
+pos = [
+        [-100 , 100], 
+        [-100, -100], 
+        [100, -100], 
+        [100, 100]
+    ]
+
+#funcion que mueve el cuadrado generado 50 espacios a la derecha
+def moveSquare(pos):
+    up()
+    clear()
+    newpos = pos
+
+    for i in range(0, 4):
+        for j in range(0, 2):
+            
+            newpos[i][j] = newpos[i][j] + 50
+    
+    
+    #dibujo del nuevo cuadrado
+    goto(newpos[0][0], newpos[0][1])
+    down()
+    goto(newpos[1][0], newpos[1][1])
+    goto(newpos[2][0], newpos[2][1])
+    goto(newpos[3][0], newpos[3][1])
+    goto(newpos[0][0], newpos[0][1])
+
+
+
+def drawSquare(pos):
+    goto(pos[0][0], pos[0][1])
+    down()
+    goto(pos[1][0], pos[1][1])
+    goto(pos[2][0], pos[2][1])
+    goto(pos[3][0], pos[3][1])
+    goto(pos[0][0], pos[0][1])
+
+    for i in range(4):
+        moveSquare(pos)
+    
+
+up()
+drawSquare(pos)
+done()
